a6f6bbee38d32d666f6e6af85aefad2d8d5593c9
shiloa3@gmail.com <shiloa3@gmail.com>
2023-11-06 22:04:14 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ProjectSidebar from "./components/ProjectSidebar";
+
+function App() {
+  return (
+    <main className="h-screen my-8">
+      <ProjectSidebar/>
+    </main>
+  );
+}
+
+export default App;
