1cba81329686a503f63b5d507d2c51ccb5a7bc04
shiloa3@gmail.com <shiloa3@gmail.com>
2023-11-07 22:44:01 +0200

    edit style

---
+++ src/components/Input.jsx
@@ -0,0 +1,8 @@
+export default function Input({ label,texterea, ...props}){
+    return(
+        <p className="flex flex-col gap-1 my-4">
+            <label className="text-sm font-bold uppercase text-stone-500">{label}</label>
+            {texterea ? <textarea className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200-"{...props} /> : <input {...props} />}
+        </p>
+    );
+}
